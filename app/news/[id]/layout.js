3370d5d4a7b93b26d23b18d89905705109539fd6
axumrs <rolson867@gmail.com>
2023-06-17 12:08:35 +0800

    新闻详情布局

---
+++ app/news/[id]/layout.js
@@ -0,0 +1,7 @@
+export default function NewsDetailLayout({ children }) {
+  return (
+    <div style={{ background: "#cffafe", padding: "10px", margin: "20px" }}>
+      {children}
+    </div>
+  );
+}
